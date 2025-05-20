43847397365ceee69bf8e446f6b177d6a28762c9
abdulqdos <abdulqdosalabinie@gmail.com>
2025-05-20 15:36:39 +0200

    Add Search Sectors Admin Dashboard

--- app/Livewire/Admin/Sectors/Index.php
+++ app/Livewire/Admin/Sectors/Index.php
@@ -5,16 +5,36 @@ namespace App\Livewire\Admin\Sectors;
 use App\Livewire\AdminComponent;
 use App\Models\Sector;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\WithPagination;
 
 #[Title('إدارة القطاعات')]
 class Index extends AdminComponent
 {
     use withPagination;
+    #[Url(as: 'q', except: '')]
+    public $searchText ;
+
+    public function clear()
+    {
+        $this->searchText = '';
+    }
+
+    public function updated()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
+        $query = Sector::query();
+
+        if(!empty($this->searchText))
+        {
+            $query->where('name', 'like', '%' . $this->searchText . '%');
+        }
         return view('livewire.admin.sectors.index' , [
-            'sectors' => Sector::paginate(10),
+            'sectors' => $query->paginate(10),
         ]);
     }
 }

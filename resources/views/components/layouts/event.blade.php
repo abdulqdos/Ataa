cc2d4072939c1d7a8c7e3ec17c515660397feb96
abdulqdos <abdulqdosalabinie@gmail.com>
2025-06-04 16:11:17 +0200

    Fix: Event icons

--- resources/views/components/layouts/event.blade.php
+++ resources/views/components/layouts/event.blade.php
@@ -2,19 +2,19 @@
     @if($event === 'created')
         <div class="flex items-center ">
             <div class="text-green-500 bg-green-100 p-1 rounded-full">
-                <i class="fas fa-plus fa-lg  text-green-500"></i>
+                <i class="fas fa-plus fa-sm text-green-500"></i>
             </div>
         </div>
     @elseif($event === 'updated')
         <div class="flex items-center text-yellow-500">
             <div class="text-yellow-500 bg-yellow-100 p-1 rounded-full">
-                <i class="fas fa-pen-square fa-lg "></i>
+                <i class="fas fa-pen-square fa-sm"></i>
             </div>
         </div>
     @elseif($event === 'deleted')
         <div class="flex items-center text-red-500">
             <div class="text-red-500 bg-red-100 p-1 rounded-full">
-                <i class="fas fa-solid fa-plus fa-lg"></i>
+                <i class="fas fa-trash-alt fa-sm"></i>
             </div>
         </div>
     @endif
